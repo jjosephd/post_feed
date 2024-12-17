02eefd8296e8523d3ad10d2d62f60ee4e78b8e3c
jjosephd <jjosephd804@gmail.com>
2024-12-16 22:08:09 -0500

    main updated with functions to handle auth

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import tweepy
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-CONSUMER_KEY = os.getenv("CONSUMER_KEY")
-CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+def get_client():
+    load_dotenv()
 
-client = tweepy.Client(
-    consumer_key=CONSUMER_KEY,
-    consumer_secret=CONSUMER_SECRET,
-    access_token=ACCESS_TOKEN,
-    access_token_secret=ACCESS_TOKEN_SECRET
-)
-
-tweet_text = "Hello, world!"
-client.create_tweet(text=tweet_text)
+    client = tweepy.Client(
+        consumer_key=os.getenv("CONSUMER_KEY"),
+        consumer_secret=os.getenv("CONSUMER_SECRET"),
+        access_token=os.getenv("ACCESS_TOKEN"),
+        access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
+    )
+    return client
+def create_tweet(client, tweet_text):
+    client.create_tweet(text=tweet_text)
+    
+client = get_client()
+create_tweet(client, "Back Again!")
+    
